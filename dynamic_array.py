e20e38f1e9099d3729be31a8d63887221ca171d2
zisankarsatar <zisankarsatar@gmail.com>
2025-02-11 23:45:54 +0100

    dynamic array

--- dynamic_array.py
+++ dynamic_array.py
@@ -25,8 +25,74 @@ class DynamicArray(object):
         self.A[self.n] = new_value #eleman ekleme
         self.n += 1 #elemena sayisini bir attir
         
-    def _resize(self):
-        ...
+    def _resize(self, new_cap):
+        B = self.make_array(new_cap) #yeni array yap
+        
+        for k in range(self.n): #eski arraydekileri yeni array a koy 
+            B[k] = self.A[k]
+            
+        self.A = B #array guncelle
+        self.capacity = new_cap #kapasite guncelle
+        
     
-    def make_array(self):
-        ...
+    def make_array(self, new_cap):
+        
+        return (new_cap*ctypes.py_object)()
+    
+new_array1 = DynamicArray()
+
+print(new_array1.capacity)
+
+new_array1.append(2)
+new_array1.append(3)
+new_array1.append(6)
+
+print(new_array1.capacity)
+
+
+def wordSplit(liste):
+    #listeyi ikiye bol
+    # ilk dizin ini harflere bol
+    # ikinci elemanin kendisini zaten liste
+    # ilk dizenin geri kalan elemanlarini ikinci dize icinde ara
+    
+    
+    word = list((liste[0]))
+    word_pool = liste[1].split(',')
+    
+    
+    #print(word);
+    #print(word_pool)
+    
+    k=0
+    aranacak_karakterler=''
+    for k in range(len(word)):
+        aranacak_karakterler += word[k]
+        geri_kalan_harfler = liste[0].split(aranacak_karakterler)[1]
+        if aranacak_karakterler in word_pool and geri_kalan_harfler in word_pool:
+            return [aranacak_karakterler, geri_kalan_harfler]
+    
+    return 'nothing'
+            
+    
+
+result = wordSplit(['deeplearn2ing', 'd,dll,a,deep,base,lear,learning'])
+
+print('anlamli kelimler : ', result)
+
+#another way
+def wordSplit2(liste):
+    word = list(liste[0])
+    d = list[1].split(',')
+    
+    for i in range(1, len(word)):
+        c = word[:] # c ile word u esitledi
+        c.insert(i,' ')
+        
+        x,y = ''.join(c).split() # 'de','eplearning'
+        if x in d and y in d:
+            return x + ' ' + y
+    
+    return 'nothing'
+
+print(wordSplit(['deeplearning', 'd,dll,a,deep,base,lear,learning']))   
