8f53f6963adbad29b9521af6f64431123a98cbcd
zisankarsatar <zisankarsatar@gmail.com>
2025-02-15 00:18:14 +0100

    doubly linked list

--- linkedList.py
+++ linkedList.py
@@ -25,3 +25,37 @@ print(istanbul.nextNode.getNodeValue())
 print(kmaras.nextNode.getNodeValue())
 print(kmaras.getNodeValue())
 print(mugla.getNodeValue())
+
+class DoublyLinkedListNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.nextNode = None
+        self.prevNode = None
+    
+    def setNextNode(self, node):
+        self.nextNode = node
+    
+    def getNextNode(self):
+        return self.nextNode
+    
+    def setPrevNode(self, node):
+        self.prevNode = node
+    
+    def getPrevNode(self):
+        return self.prevNode
+    
+    def getNodeValue(self):
+        return self.value
+    
+netherlands = DoublyLinkedListNode('ams')
+belgium = DoublyLinkedListNode('brussel')
+germany = DoublyLinkedListNode('berliin')
+
+netherlands.setNextNode(belgium)
+belgium.setPrevNode(netherlands)
+belgium.setNextNode(germany)
+germany.setPrevNode(belgium)
+
+print(netherlands.getNodeValue())
+print(belgium.getNextNode().getNodeValue())
+print(germany.getPrevNode().getPrevNode().getNodeValue())
