be7d60156aa37dc60211b2c3673147cf3f0d33e6
zisankarsatar <zisankarsatar@gmail.com>
2025-02-14 23:57:12 +0100

    linked list

---
+++ linkedList.py
@@ -0,0 +1,26 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.nextNode = None
+        
+    def setNextNode(self, node):
+        self.nextNode = node
+    
+    def getNextNode(self):
+        return self.nextNode
+    
+    def getNodeValue(self):
+        return self.value
+    
+
+istanbul = Node('34')
+kmaras = Node('46')
+mugla = Node('48')
+
+istanbul.setNextNode(kmaras)
+kmaras.setNextNode(mugla)
+
+print(istanbul.getNodeValue())
+print(istanbul.nextNode.getNodeValue())
+print(kmaras.nextNode.getNodeValue())
+print(mugla.getNodeValue())
