6262b724c7a7df905f32a63a6bd39a41501098b9
zisankarsatar <zisankarsatar@gmail.com>
2025-02-17 22:29:49 +0100

    insert data in linkedlist

--- linkedList.py
+++ linkedList.py
@@ -59,3 +59,51 @@ germany.setPrevNode(belgium)
 print(netherlands.getNodeValue())
 print(belgium.getNextNode().getNodeValue())
 print(germany.getPrevNode().getPrevNode().getNodeValue())
+
+
+class linkedList(object):
+    def __init__(self):
+        self.head = None
+        
+    def push(self, new_data):
+        new_node = Node(new_data)
+        
+        new_node.nextNode = self.head
+        
+        self.head = new_node
+        
+    def insertAfter(self,prev_node, new_data):
+        
+        #prev node var mi ?
+        if prev_node is None:
+            print('boyle bi data yok')
+            return
+    
+        # yeni node yarat ve icine yerlestir
+        new_node = Node(new_data)
+        
+        # new_node next in prev yap
+        new_node.nextNode = prev_node.nextNode
+        
+        #prev node nextin new node yap
+        prev_node.nextNode = new_node
+        
+    def printLinkedList(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.nextNode
+            
+linked_list = linkedList() 
+linked_list.push('son eleman')
+linked_list.push(25)
+linked_list.push(15)
+linked_list.push(5)
+linked_list.push('ilk eleman')
+
+linked_list.printLinkedList()
+
+linked_list.insertAfter(linked_list.head.nextNode, 35)
+linked_list.insertAfter(linked_list.head, 45)
+
+linked_list.printLinkedList()
