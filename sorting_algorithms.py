abf1de0de992ca9c88ec1a4028844aeb7d2fcba3
zisankarsatar <zisankarsatar@gmail.com>
2025-02-22 18:38:28 +0100

    merge sorting

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -9,4 +9,40 @@ def bubble_sort(arr):
                 
     return arr
 
-print(bubble_sort([2,1,56,7,89,6,45,99,134]))
+#print(bubble_sort([2,1,56,7,89,6,45,99,134]))
+
+def merge_sorting(arr):
+    if len(arr) > 1:
+        mid = int(len(arr)/2)
+        lefthalf = arr[:mid]
+        righthalf = arr[mid:]
+        
+        merge_sorting(lefthalf)
+        merge_sorting(righthalf)
+        
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                arr[k] = lefthalf[i]
+                i += 1
+            else:
+                arr[k] = righthalf[j]
+                j += 1
+            k += 1
+        
+        while i < len(lefthalf):
+            arr[k] = lefthalf[i] 
+            i += 1
+            k += 1
+        
+        while j < len(righthalf):
+            arr[k] = righthalf[j] 
+            j += 1
+            k += 1
+        
+    return arr
+
+print(merge_sorting([12,7,8,6,1]))
+               
