1005ab89730a32757e0ef663cd43383658ed429d
zisankarsatar <zisankarsatar@gmail.com>
2025-02-23 22:37:43 +0100

    quick sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -109,4 +109,50 @@ def counting_sorting(arr, maxval):
     
     return arr
 
-print(counting_sorting([1,1,1,2,2,3,4,2,5,6,3,4,6,3,3,7,1], 7))
+#print(counting_sorting([1,1,1,2,2,3,4,2,5,6,3,4,6,3,3,7,1], 7))
+
+#recursive
+def quick_sort(arr):
+    quick_sort_recursion(arr, 0, len(arr)-1)
+    return arr
+
+def quick_sort_recursion(arr, first, last):
+    
+    if first < last:
+        splitpoint = partition(arr, first, last)
+        
+        #left right
+        
+        quick_sort_recursion(arr, first, splitpoint-1)
+        quick_sort_recursion(arr, splitpoint+1, last)
+        
+def partition(arr, first, last):
+    #pivot value
+    pivotvalue = arr[first]
+    
+    left = first+1
+    right = last
+    
+    done = False
+    
+    while not done:
+        while left <= right and arr[left] <= pivotvalue:
+            left = left+1
+        while arr[right] >= pivotvalue and right >= left:
+            right = right -1
+        
+        if right < left:
+            done = True
+        else:
+            temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp
+        
+        temp = arr[first]
+        arr[first] = arr[right]
+        arr[right] = temp
+    
+    return right
+
+arr = [3,2,13,4]
+print(quick_sort(arr))
