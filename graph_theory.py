681d7a44786e613e83184c9ad73cc02e12bbcafa
zisankarsatar <zisankarsatar@gmail.com>
2025-03-02 14:08:54 +0100

    graph has been added

--- graph_theory.py
+++ graph_theory.py
@@ -17,3 +17,42 @@ class Vertex:
 
     def getWeight(self, neighbor):
         return self.connectedTo[neighbor]
+
+class Graph:
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+    
+    def addVertex(self,key):
+        newVertx = Vertex(key)
+        self.vertList[key] = newVertx
+        self.numVertices += 1
+        return newVertx
+   
+    def getVertex(self, n):
+        if n is self.vertList:
+           return self.vertList[n]
+        else:
+            return None
+    
+    def __contains__(self, n):
+        return n in self.vertList
+    
+    def addEdge(self, f, t, cost=0):
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+        
+    def getVertices(self):
+        return self.vertList.keys()
+    
+    def __iter__(self):
+        return iter(self.vertList.values())
+#addVertex(vert) -> graph icerisine node(vertex) ekler
+#addEdge(fromVert, toVert) -> iki node birbinine baglayan directed edge ekler 
+#addEdge(fromVert, toVert, weight) -> iki node birbinine baglayan weighted and directed edge ekler 
+#getVertex(vertexKey) -> graph icerisinde node bulur
+#getVertices() -> node lari return eder 
+
