ed995a227b9fbfb8d002bea8253b1fc96f99d796
zisankarsatar <zisankarsatar@gmail.com>
2025-03-14 23:50:32 +0100

    exp

--- sieve.py
+++ sieve.py
@@ -1,3 +1,10 @@
+"""
+### Generate Primes Up To N Using Sieve of Eratosthenes Algorithm
+•⁠  ⁠The sieve of Eratosthenes algorithm generates all the primes up to a given limit. 
+This is a common and fast algorithe used to generate a list of primes up to a given limit. It works by making 
+a list from 1 to N, and then iterating through the list and progressively removing non-prime, composite numbers 
+until only primes are left in a list.
+"""
 def sieve(limit):
     bools = []
     primes = []
