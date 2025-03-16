548b85c9e85b812e05f1be7ead3463882f00f5dd
zisankarsatar <zisankarsatar@gmail.com>
2025-03-16 23:48:25 +0100

    np array ozellikleri

---
+++ numoy_tr/numpy_array.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+#basic array
+a = np.array([3,4,5,2])
+print(type(a))
+
+a = np.array([3.14,4,5,2])
+print(a)
+# cikti -->> [3.14 4.   5.   2.  ] 
+#herhangi bir eleman ondakli ise digerleride ondakliya ceviriliyor. Numpy array in sabit veri tipi ozelliginden dolayi
+a = np.array([3.14,4,5,2], dtype='int')
+print(a)
+#integer a ceriliyor tum liste elemanlari
+
+a = np.zeros(10)
+print(a)
+#sifirdan 10 elemanli elemanlarinin herbiri 0 olan bir liste
+
+a = np.ones((3,5), dtype='int')
+print(a)
+# 3 satir, 5 sutunluk 1 den olusan matris
+
+a = np.full((3,5), 3)
+print(a)
+# 3 satir, 5 sutunluk 3 den olusan matris
+
+a = np.arange(0,31,3)
+print(a)
+# 0 dan 31 e kadar 3 er 3 er artan bir liste
+
+a = np.linspace(0,1,10)
+print(a)
+# Odan 1e 10 sayi
+
+a  = np.random.normal(10,4,(3,5))
+print(a)
+#ortalamasi 10, standart sapmasi 4 olan 3e 5lik matris
+
+a  = np.random.randint(0,10,(3,4))
+print(a)
+# 0dan 10a 3,4luk integer matirs
