bd02cc2ebc85da051c538c46f00a35310b158217
zisankarsatar <zisankarsatar@gmail.com>
2025-03-24 22:52:41 +0100

    space are removed

--- numpy_tr/condition.py
+++ numpy_tr/condition.py
@@ -4,24 +4,17 @@ v = np.array([1,2,3,4,5,6])
 
 print(v<3)
 #[True True False False False False]
-
 print(v[v<3])
 #[1,2]
-
 print(v[v>=3])
 #[3,4,5,6]
-
 print(v[v != 3])
 #[1,2,4,5,6]
-
 print(v[v == 3])
 #[3]
-
 print(v*2)
 #[ 2  4  6  8 10 12]
-
 print(v/5)
 #[0.2 0.4 0.6 0.8 1.  1.2]
-
 print(v**2)
 #[ 1  4  9 16 25 36]
