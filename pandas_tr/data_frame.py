b75c2009aff2b5650283cf81f1c6dd2b6665c3f5
zisankarsatar <zisankarsatar@gmail.com>
2025-04-01 16:28:51 +0200

    commit

--- pandas_tr/data_frame.py
+++ pandas_tr/data_frame.py
@@ -4,10 +4,9 @@ import numpy as np
 liste = [1,2,45,6,4,3]
 
 pd.DataFrame(liste, columns=['degisken ismi'])
-
 arr = np.arange(1,10).reshape(3,3)
 df = pd.DataFrame(arr, columns=['var1','var2','var3'])
-print(df)
+#print(df)
 """
    var1  var2  var3
 0     1     2     3
@@ -15,17 +14,17 @@ print(df)
 2     7     8     9
 """
 df.columns = ('deg1','deg2','deg3')
-print(df)
+#print(df)
 """
    deg1  deg2  deg3
 0     1     2     3
 1     4     5     6
 2     7     8     9
 """
-print(type(df))
-print(df.axes)
-print(df.shape)
-print(df.ndim)
-print(df.values)
-print(df.head())
-print(df.tail(1))
+#print(type(df))
+#print(df.axes)
+#print(df.shape)
+#print(df.ndim)
+#print(df.values)
+#print(df.head())
+#print(df.tail(1))
