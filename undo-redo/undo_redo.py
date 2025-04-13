2e941cac97380ff4026549b0f8bf46967a26423c
zisankarsatar <zisankarsatar@gmail.com>
2025-04-13 17:57:08 +0200

    user input func added

--- undo-redo/undo_redo.py
+++ undo-redo/undo_redo.py
@@ -3,9 +3,9 @@ class UndoRedo():
         self.undo_stack = []
         self.redo_stack = []
 
-        self.undo_stack.append('zisan')
-        self.undo_stack.append('zisan karsatar')
-        self.undo_stack.append('zisan karsatar caliskan')
+        # self.undo_stack.append('zisan')
+        # self.undo_stack.append('zisan karsatar')
+        # self.undo_stack.append('zisan karsatar caliskan')
 
     def append_new(self, text):
         self.undo_stack.append(text)
@@ -15,21 +15,27 @@ class UndoRedo():
             current = self.undo_stack.pop()
             self.redo_stack.append(current)
             print(self.undo_stack[-1])
-            
+        else:
+            print('finished')
+        
     def redo(self):
         if self.redo_stack != []:
             current = self.redo_stack.pop()
             self.undo_stack.append(current)
             print(self.undo_stack[-1])
+        else:
+            print('finished')
 
 test = UndoRedo()
-test.append_new('canim')
-test.append_new('traveller')
-test.undo() #geri almak
-test.undo() #geri almak
-test.undo() #geri almak
-test.undo() #geri almak
-test.redo()
-test.redo()
-test.redo()
-test.redo()
+text = input('Pls write something: ')
+test.append_new(text)
+text = input('Pls write something: ')
+test.append_new(text)
+text = input('Pls write something: ')
+test.append_new(text)
+
+step = input('Undo or Redo(u/r):' )
+if step == 'u':
+    test.undo() #geri almak
+else:
+   test.redo()     
